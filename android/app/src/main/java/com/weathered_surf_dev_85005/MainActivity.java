7cced77f452fea5e4a8134e1ce8211a913060986
Crowdbotics <engineering@crowdbotics.com>
2023-05-21 00:56:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/weathered_surf_dev_85005/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weathered_surf_dev_85005;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "weathered_surf_dev_85005";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-surf-dev-85005",
+  url: "https://weathered-surf-dev-85005.botics.co",
+  api: "https://weathered-surf-dev-85005.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
